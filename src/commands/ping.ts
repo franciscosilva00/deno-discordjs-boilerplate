2e760e31335cf791d8fc85d052b770e47f2a642e
Francisco Silva <1221518@isep.ipp.pt>
2023-08-14 05:39:05 +0100

    :bug: fixed type for commands (CommandInteraction)

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,11 @@
+import type { CommandInteraction } from "npm:discord.js@14.12.1";
 import { SlashCommandBuilder } from "npm:discord.js@14.12.1";
-import type { Interaction } from "npm:discord.js@14.12.1";
 
 export const data = new SlashCommandBuilder()
   .setName("ping")
   .setDescription("Replies with Pong!");
 
-export const execute = async (interaction: Interaction) => {
+export const execute = async (interaction: CommandInteraction) => {
   if (!interaction.isRepliable()) return;
 
   await interaction.reply("Pong!");
